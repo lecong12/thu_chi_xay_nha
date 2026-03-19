466c2f097695c148ea182b13fcdbb46705b6a1a8
Lê Công <lecong12@gmail.com>
2026-03-19 18:49:15 +0700

    cập nhật

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-// Chuyển tiếp request từ Vercel (thư mục api) sang file index.js ở root
-const app = require('../index.js');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-module.exports = app;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
