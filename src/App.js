29d6bbe843fd127eea69e19f6d04f80dc23b3177
Lê Công <lecong12@gmail.com>
2026-03-24 10:54:26 +0700

    cập nhật

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import Dashboard from "./components/Dashboard";
 
 // --- Tách Component để dễ quản lý ---
